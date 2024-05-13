1a03b46ca36883c07e9b919cbaef0db2fc6f8bd5
JM Lugo <lugoc.jose@outlook.com>
2024-05-12 20:33:04 -0700

    Exported all components on barrel file

---
+++ components/index.ts
@@ -0,0 +1,12 @@
+export { AddSaleItemModal } from "./sales/AddSaleItemModal";
+export { ConfirmDeleteModal } from "./products/ConfirmDeleteModal";
+export { EditIcon } from "./ui/EditIcon";
+export { InventoryTable } from "./products/InventoryTable";
+export { Navbar } from "./ui/Navbar";
+export { NavBarLink } from "./ui/NavBarLink";
+export { NewSaleTable } from "./sales/NewSaleTable";
+export { ProductModal } from "./products/ProductModal";
+export { SaleInformations } from "./sales/SaleInformations";
+export { SalesTable } from "./sales/SalesTable";
+export { ThemeSwitch } from "./ui/Theme-Switch";
+export * from "./ui/Icons"
