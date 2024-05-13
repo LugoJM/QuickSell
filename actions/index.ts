ea46112c4e1fb17aa48c43bcfb8f575f9ffd686a
JM Lugo <lugoc.jose@outlook.com>
2024-05-12 20:28:50 -0700

    Export all server actions on barrel file

---
+++ actions/index.ts
@@ -0,0 +1,7 @@
+export { createProduct } from "./product/create-product";
+export { createSale } from "./sale/create-sale";
+export { deleteProduct } from "./product/delete-product";
+export { getProducts } from "./product/get-products";
+export { getSaleById } from "./sale/get-saleById";
+export { getSales } from "./sale/get-sales";
+export { updateProduct } from "./product/update-product";
